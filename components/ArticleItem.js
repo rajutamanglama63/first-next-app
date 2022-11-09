2701bcb2ab5d64a729e84fe7929519b23d7c0db0
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-11-09 23:32:06 +0545

    feat: displaying article page done

---
+++ components/ArticleItem.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import articleStyles from "../styles/Articles.module.css";
+
+const ArticleItem = ({ article }) => {
+  return (
+    <Link href="/article/[id]" as={`/article/${article.id}`}>
+      <div className={articleStyles.card}>
+        <h3>{article.title} &rarr;</h3>
+        <p>{article.body}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default ArticleItem;
